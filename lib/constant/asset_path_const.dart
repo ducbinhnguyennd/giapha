225af0c6edcb737c7d16aa0034b5fff6278a3382
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2024-01-09 17:45:09 +0700

    update code

---
+++ lib/constant/asset_path_const.dart
@@ -0,0 +1,27 @@
+class AssetsPathConst {
+  static const String splashBackgroundNgonTinh = 'assets/images/icons.png';
+  static const String backgroundStoryDetail = 'assets/images/ico_free.png';
+
+  static const String gifloading = 'assets/images/gifloading.gif';
+  static const String ico_back = 'assets/images/ico_back.png';
+
+  //khung
+  static const String khunglevel1 = 'assets/images/khunglevel1.png';
+  static const String khunglevel2 = 'assets/images/khunglevel2.png';
+  static const String khunglevel4 = 'assets/images/khunglevel4.png';
+
+  static const String categoryBell = 'assets/images/chuong.png';
+  static const String tabUser = 'assets/images/tab_user.png';
+  static const String tabCategory = 'assets/images/tab_category.png';
+  static const String tabHome = 'assets/images/tab_home.png';
+  static const String tabBook = 'assets/images/tab_book.png';
+  static const String tabTop = 'assets/images/tab_shop.png';
+
+  static const String nen = 'assets/images/nen.png';
+
+//icons
+  static const String logo = 'assets/images/icons.png';
+  static const String tichxanh = 'assets/images/tichxanh.png';
+
+  static const String ico_face = 'assets/images/icosetting/ico_face.png';
+}
